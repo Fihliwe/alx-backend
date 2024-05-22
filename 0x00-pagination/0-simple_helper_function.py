e7b3a1aec163379a4a693b4227f378e2437df448
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-05-22 10:42:25 +0000

    init commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 Simple helper function
 """
 
+
 def index_range(page, page_size):
     """
     The function should return a tuple of size two containing a start index and an end index corresponding to the range of indexes 
